87df1c62bc37971d2630565a4745d33c44008938
humbertyao <humbertyao@tencent.com>
2020-11-10 23:44:01 +0800

    appium元素定位

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : test.py
+# Author: yaojunjie
+# Date  : 2020/11/10
+from appium import webdriver
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '6.0'
+desired_caps['deviceName'] = 'emulator-5554'
+desired_caps['appPackage'] = 'com.android.settings'
+desired_caps['appActivity'] = 'com.android.settings.Settings'
+driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+driver.quit()
