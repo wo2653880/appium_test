710f9467cb90d5f9de8e2748c46ac82d6df10ee5
humbertyao <humbertyao@tencent.com>
2020-11-12 23:15:07 +0800

    录制脚本

---
+++ test_record.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : test_recording.py
+# Author: yaojunjie
+# Date  : 2020/11/10
+from appium import webdriver
+
+desired_cap = {
+  "platformName": "android",
+  "deviceName": "127.0.0.1:7555",
+  "appPackage": "com.xueqiu.android",
+  "appActivity": ".view.WelcomeActivityAlias",
+  "noReset": True
+}
+
+driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_cap)
+driver.implicitly_wait(20)
+el = driver.find_element_by_id("com.xueqiu.android:id/tv_search")
+el.click()
+el1 = driver.find_element_by_id("com.xueqiu.android:id/search_input_text")
+el1.send_keys("腾讯")
+el2 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget"
+                                   ".LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget"
+                                   ".FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android"
+                                   ".widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget"
+                                   ".RecyclerView/android.widget.RelativeLayout[1]")
+el2.click()
