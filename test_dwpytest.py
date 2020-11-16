2e7160f44656903f40bea7cfc0941f4221adf2c2
humbertyao <humbertyao@tencent.com>
2020-11-17 00:34:01 +0800

    新增test_attribute

--- test_dwpytest.py
+++ test_dwpytest.py
@@ -31,6 +31,7 @@ class TestDW:
         sleep(3)
         self.driver.quit()
 
+    @pytest.mark.skip
     def test_search(self):
         """
         1.打开雪球app
@@ -46,6 +47,32 @@ class TestDW:
         el2.click()
         current_price = float(self.driver.find_element_by_id("com.xueqiu.android:id/current_price").text)
         assert current_price > 200
+
+    def test_attr(self):
+        """
+        打开[雪球]应用app
+        定位首页搜索框
+        判断搜索框是否可用，并查看搜索康name值
+        打印搜索框这个元素左上角坐标和它的宽高
+        像搜索框输入腾讯
+        判断“腾讯”是否可见
+        如果可见，打印搜索成功 反之打印搜索失败
+        """
+        el = self.driver.find_element_by_id("com.xueqiu.android:id/tv_search")
+        search_enabled = el.is_enabled()
+        print(el.text)
+        print(el.location)
+        print(el.size)
+        if search_enabled:
+            el.click()
+            el1 = self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text")
+            el1.send_keys("腾讯")
+            el2 = self.driver.find_element_by_xpath("//*[@resource-id='com.xueqiu.android:id/name' and @text='腾讯控股']")
+            if el2.get_attribute("displayed") == 'true':
+                print("success")
+            else:
+                print("fail")
+
 
 
 if __name__ == '__main__':
